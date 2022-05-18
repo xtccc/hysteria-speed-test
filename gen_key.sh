3212d45e1c6c9c2689fec5af19f001cf4bdf6f65
xtccc <small_parking@outlook.com>
2022-05-18 23:35:05 +0800

    test workflows

---
+++ gen_key.sh
@@ -0,0 +1,7 @@
+openssl genrsa -aes256 -passout pass:gsahdg -out server.pass.key 4096
+openssl rsa -passin pass:gsahdg -in server.pass.key -out server.key
+rm server.pass.key
+
+openssl req -new -key server.key -out server.csr -subj "/CN=127.0.0.1"
+
+openssl x509 -req -sha256 -days 365 -in server.csr -signkey server.key -out server.crt
---
+++ run_hy_speedtest.sh
@@ -0,0 +1,3 @@
+ ./hysteria-linux-amd64 server -c ./server.json &
+  ./hysteria-linux-amd64 client -c ./client.json &
+./speedtest-backend &
