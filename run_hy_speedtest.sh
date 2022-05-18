d1c38c6ac287649f8b9901cc5b3c99bb790b26bf
xtccc <small_parking@outlook.com>
2022-05-18 23:47:38 +0800

    test workflows

--- run_hy_speedtest.sh
+++ run_hy_speedtest.sh
@@ -1,8 +1,6 @@
 ls
 pwd
-find .|| echo "not find"
-
-
- ./hysteria-linux-amd64 server -c ./server.json &
-  ./hysteria-linux-amd64 client -c ./client.json &
+find .
+./hysteria-linux-amd64 server -c ./server.json &
+./hysteria-linux-amd64 client -c ./client.json &
 ./speedtest-backend &
