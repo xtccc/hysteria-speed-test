434e2cfcdc6e6bbd49a5d0031592d194d330733d
small_parking <small_parking@outlook.com>
2022-05-19 00:51:41 +0800

    Update run_hy_speedtest.sh

--- run_hy_speedtest.sh
+++ run_hy_speedtest.sh
@@ -1,6 +1,8 @@
 ls
 pwd
 find .
+top -c  -b &> top.log &
 ./hysteria-linux-amd64 server -c ./server.json &> server.log &
 ./hysteria-linux-amd64 client -c ./client.json &> client.log &
 ./speedtest-backend &
+
