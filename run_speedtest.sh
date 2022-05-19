131605e8a22ac1d65b7f49b5b7dcce590de9cea8
xtccc <small_parking@outlook.com>
2022-05-19 18:14:08 +0800

    add wget log

--- run_speedtest.sh
+++ run_speedtest.sh
@@ -42,7 +42,7 @@ echo "after"
 sed -n "/$ip/p" main.go
 #./sed_the_ip.sh end
 go build -v ./...
-wget https://github.com/HyNetwork/hysteria/releases/download/v1.0.4/hysteria-linux-amd64 && chmod +x hysteria-linux-amd64
+wget https://github.com/HyNetwork/hysteria/releases/download/v1.0.4/hysteria-linux-amd64 -o down_hy.log && chmod +x hysteria-linux-amd64
 
 #./gen_key.sh
 openssl genrsa -aes256 -passout pass:gsahdg -out server.pass.key 4096
@@ -52,7 +52,7 @@ openssl req -new -key server.key -out server.csr -subj "/CN=ip"
 openssl x509 -req -sha256 -days 365 -in server.csr -signkey server.key -out server.crt
 #./gen_key.sh end
 
-wget https://github.com/librespeed/speedtest-go/releases/download/v1.1.4/speedtest-go_1.1.4_linux_amd64.tar.gz && tar xf speedtest-go_1.1.4_linux_amd64.tar.gz && rm speedtest-go_1.1.4_linux_amd64.tar.gz
+wget https://github.com/librespeed/speedtest-go/releases/download/v1.1.4/speedtest-go_1.1.4_linux_amd64.tar.gz -o down_speedtest_go.log  && tar xf speedtest-go_1.1.4_linux_amd64.tar.gz && rm speedtest-go_1.1.4_linux_amd64.tar.gz
 #./run_hy_speedtest.sh
 lscpu
 
